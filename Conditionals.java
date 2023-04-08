a8131894340f556656f2e1981f96f10e3ab595c2
Emrecan Ozkan <iletisim@paradyo.com>
2023-04-07 18:30:04 -0700

    Conditionals and loops

---
+++ Conditionals.java
@@ -0,0 +1,59 @@
+public class Conditionals {
+    public static void main(String[] args)
+    {
+        //if else , switch
+
+        /*
+        byte studentScore = 48;
+
+        if(studentScore > 60){
+            //you passed
+            System.out.println("you passed");
+        }else if(studentScore > 50 && studentScore <= 60){
+            // you gotta take one more test.
+            System.out.println("you gotta take one more test.");
+        }
+        else{
+            //you couldn't pass.
+            System.out.println("you couldn't pass");
+        }
+        */
+
+        //1 -> January
+        //2 -> February
+        /*
+        ...
+        .
+        ...
+        ..
+        //11 -> November
+        ///or December
+
+
+        byte monthNumber = 12;
+
+        switch (monthNumber)
+        {
+            case 1:
+                // do this
+                System.out.println("January");
+                break;
+            case 2:
+                // do this
+                System.out.println("February");
+                break;
+            case 11:
+                // do this
+                System.out.println("November");
+                break;
+            default:
+                //default
+                System.out.println("December");
+                break;
+        }
+
+         */
+
+
+    }
+}
---
+++ Loops.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args)
+    {
+        //write the numbers between 1 to 5
+
+        /*
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+            if(i == 3)
+            {
+                System.out.println("I am 3!");
+            }
+        }
+
+
+        Scanner in = new Scanner(System.in);
+        int numberFromUser = Integer.parseInt(in.next());
+
+        while(numberFromUser < 5)
+        {
+            //do this
+            System.out.println(numberFromUser);
+            numberFromUser++;
+        }
+
+        Scanner in = new Scanner(System.in);
+        int numberFromUser = Integer.parseInt(in.next());
+
+
+        do {
+            //do this
+            System.out.println(numberFromUser);
+            numberFromUser++;
+        } while(numberFromUser < 1);
+        */
+    }
+}
