edba33dbccba7ea58afb5b3b270fd3037a09d6ce
Emrecan Ozkan <iletisim@paradyo.com>
2023-04-09 10:37:50 -0700

    exception handling

---
+++ Exceptions.java
@@ -0,0 +1,22 @@
+public class Exceptions {
+    public static void main(String[] args) throws MyBeautifulException {
+        ///something hppens here in this method.
+        //and it occured an error which is MyBeautifulException
+        //throw new MyBeautifulException("There is a specific problem.");
+
+        //String name = "Emrecan";
+
+        /*
+        try {
+            int a = 10;
+            int b = 0;
+            int c = a / b;
+        }catch (ArithmeticException e){
+            System.out.println(e);
+        } finally{
+            System.out.println("Thats finally");
+        }
+         */
+
+    }
+}
---
+++ MyBeautifulException.java
@@ -0,0 +1,5 @@
+public class MyBeautifulException extends Exception {
+    MyBeautifulException(String s){
+        System.out.println(s);
+    }
+}
